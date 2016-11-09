5b3dce7f94d9da596d7f1c74d48fc46b75464d30
“Archit <190902@hkis.edu.hk>
2016-11-09 14:25:18 +0800

    Added loop
    2nd Loop not working as of now

--- src/EnigmaTest.java
+++ src/EnigmaTest.java
@@ -1,97 +1,121 @@
 import java.util.*;
+
 public class EnigmaTest {
-private static String message;
-public static void main(String args[]){
-	Scanner input = new Scanner(System.in);
-	System.out.println("Gimme some text to encrypt!");
-	message = input.nextLine();
-	String revmes = new StringBuffer(EnigmaTest.getMessage()).reverse().toString();
-	System.out.println(revmes);
-	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-	map = new HashMap<>();  
-	//this maps char a to int 1 and so on
-    map.put('a', 1);
-    map.put('b', 2);
-    map.put('c', 3);
-    map.put('d', 4);
-    map.put('e', 5);
-    map.put('f', 6);
-    map.put('g', 7);
-    map.put('h', 8);
-    map.put('i', 9);
-    map.put('j', 10);
-    map.put('k', 11);
-    map.put('l', 12);
-    map.put('m', 13);
-    map.put('n', 14);
-    map.put('o', 15);
-    map.put('p', 16);
-    map.put('q', 17);
-    map.put('r', 18);
-    map.put('s', 19);
-    map.put('t', 20);
-    map.put('u', 21);
-    map.put('v', 22);
-    map.put('w', 23);
-    map.put('x', 24);
-    map.put('y', 25);
-    map.put('z', 26);
-    map.put(' ', 27);
-    for(final char c : revmes.toCharArray())
-    {
-        Integer values;
+	private static String message;
 
-        values = map.get(c);
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Gimme some text to encrypt!");
+		message = input.nextLine();
+		message = "bobo "+message +" bobo";
+		//System.out.println(message);
+		String revmes = new StringBuffer(EnigmaTest.getMessage()).reverse().toString();
+		System.out.println(revmes);
+		HashMap<Character, Character> map = new HashMap<Character, Character>();
+		map = new HashMap<>();
+		// this use hashmap method to map char a to int 1 and so on
+		map.put('a', '!');
+		map.put('b', '@');
+		map.put('c', '#');
+		map.put('d', '$');
+		map.put('e', '%');
+		map.put('f', '^');
+		map.put('g', '&');
+		map.put('h', '*');
+		map.put('i', '(');
+		map.put('j', ')');
+		map.put('k', '-');
+		map.put('l', '_');
+		map.put('m', '=');
+		map.put('n', '+');
+		map.put('o', '[');
+		map.put('p', ']');
+		map.put('q', '{');
+		map.put('r', '}');
+		map.put('s', '|');
+		map.put('t', ';');
+		map.put('u', ':');
+		map.put('v', '/');
+		map.put('w', '.');
+		map.put('x', ',');
+		map.put('y', '<');
+		map.put('z', '>');
+		map.put(' ', '?');
+		char[] numbers = new char[message.length()];
+		
+		int count = 0;
+		for (final char c : revmes.toCharArray()) {
 
-        if(values == null)
-        {   
-            // some sorta error
-        }
-        else
-        {
-            System.out.print(values + " ");
-            //prints out int values from the revmes char array which is cool
-        }
-    }
-    System.out.println("\nDoes u want me to decrypt this gibberish?");
-    String potato = input.nextLine();
-   if(potato .equalsIgnoreCase("yes")){
-	   HashMap<Integer, Character> tap = new HashMap<Integer, Character>();
-	   tap = new HashMap<>();  
-       tap.put(1,'a');
-       tap.put(2,'b');
-       tap.put(3,'c');
-       tap.put(4,'d');
-       tap.put(5,'e');
-       tap.put(6,'f');
-       tap.put(7,'g');
-       tap.put(8,'h');
-       tap.put(9,'i');
-       tap.put(10,'j');
-       tap.put(11,'k');
-       tap.put(12,'l');
-       tap.put(13,'m');
-       tap.put(14,'n');
-       tap.put(15,'o');
-       tap.put(16,'p');
-       tap.put(17,'q');
-       tap.put(18,'r');
-       tap.put(19,'s'); 
-       tap.put(20,'t');
-       tap.put(21,'u');
-       tap.put(22,'v');
-       tap.put(23,'w');
-       tap.put(24,'x');
-       tap.put(25,'y');
-       tap.put(26,'z');
-       tap.put(27,' ');   
-       System.out.println("This is a checker");
-   }else{
-	   System.out.println("Whatcha doing with those numbers? Oh well");
-   }
-}
-public static String getMessage() {
-	return EnigmaTest.message;
-}
+			char values = map.get(c);
+
+			if (values == 0) {
+				// some sorta error
+			} else {
+				// System.out.print(values + " ");
+				// prints out int values from the revmes char array which is cool
+				char bobo = values;
+				System.out.print(bobo);	
+			}
+			}
+		System.out.println("\nInput the text above to decrypt!");
+		String weird = input.nextLine();
+		//String[] strArray = new String[] {weird};
+		//System.out.println("\n"+strArray[1]);
+		String[] s = weird.split("");
+		String pototo = Arrays.toString(s);
+		System.out.println(pototo);
+	
+	System.out.println("Does u want me to decrypt this gibberish?");
+		String potato = input.nextLine();
+		if (potato.equalsIgnoreCase("yes") || potato.equalsIgnoreCase("oui")) {
+			HashMap<Character, Character> tap = new HashMap<Character, Character>();
+			tap = new HashMap<>();
+			tap.put('!', 'a');
+			tap.put('@', 'b');
+			tap.put('#', 'c');
+			tap.put('$', 'd');
+			tap.put('%', 'e');
+			tap.put('^', 'f');
+			tap.put('&', 'g');
+			tap.put('*', 'h');
+			tap.put('(', 'i');
+			tap.put(')', 'j');
+			tap.put('-', 'k');
+			tap.put('_', 'l');
+			tap.put('=', 'm');
+			tap.put('+', 'n');
+			tap.put('[', 'o');
+			tap.put(']', 'p');
+			tap.put('{', 'q');
+			tap.put('}', 'r');
+			tap.put('|', 's');
+			tap.put(';', 't');
+			tap.put(':', 'u');
+			tap.put('/', 'v');
+			tap.put('.', 'w');
+			tap.put(',', 'x');
+			tap.put('<', 'y');
+			tap.put('>', 'z');
+			tap.put('?', ' ');
+			for (final char d : pototo.toCharArray()) {
+				char dalues = tap.get(d);
+				System.out.println("test");
+				if (dalues == 0) {
+					// some sorta error
+				} else {
+					// System.out.print(values + " ");
+					// prints out int values from the revmes char array which is cool
+					char dobo = dalues;
+					System.out.println(dobo);	
+				}
+				}
+			System.out.println("This is a checker");
+		} else {
+			System.out.println("Whatcha doing with those numbers? Oh well");
+		}
 }
 
+	public static String getMessage() {
+		return EnigmaTest.message;
+	}
+}
