b5d95286274c931b41adb7078979313b59076171
“Archit <190902@hkis.edu.hk>
2016-11-12 22:20:39 +0800

    Finally made the code into one class rather just focused on 1 class
    Woohoo

--- src/Enigma.java
+++ src/Enigma.java
@@ -1,12 +1,75 @@
 import java.util.HashMap;
 import java.util.Map;
 public class Enigma {
-public static String revmes;
-
-
-public Enigma(){
+	
+public String hello(){
+	return "Hello, What do you want me to encrypt?";
 }
-public static void main(){
-	//System.out.println(EnigmaTest.pototo);
+public String glgl(String message){
+	return "glgl "+message+" glgl";
 }
+public String reverse(String message){
+	return new StringBuffer(message).reverse().toString();
+}
+public void encrypt(String message){
+	HashMap<Character, Character> map = new HashMap<Character, Character>();
+	map = new HashMap<>();
+	// this use hashmap method to map char a to special characters such as ! and @ and so on
+	map.put('a', '!');map.put('b', '@');
+	map.put('c', '#');map.put('d', '$');
+	map.put('e', '%');map.put('f', '^');
+	map.put('g', '&');map.put('h', '*');
+	map.put('i', '(');map.put('j', ')');
+	map.put('k', '-');map.put('l', '_');
+	map.put('m', '=');map.put('n', '+');
+	map.put('o', '[');map.put('p', ']');
+	map.put('q', '{');map.put('r', '}');
+	map.put('s', '|');map.put('t', ';');
+	map.put('u', ':');map.put('v', '/');
+	map.put('w', '.');map.put('x', 'ç');
+	map.put('y', '<');map.put('z', '>');
+	map.put(' ', '?');
+	for (final char c : message.toCharArray()) {
+		char values = map.get(c);
+		if (values == 0) {
+			//OH OH
+			// some sorta error
+		} else {
+			// System.out.print(values + " ");
+			// prints out special values from the revmes char array which is cool
+			char dobo = values;
+			System.out.print(dobo);
+		}	
+}	
+//bracket for this encryption method closes
+	}
+public String oui(){
+	return "You wanna decrypt this?";
+}
+
+	public void decryption(String message){
+	HashMap<Character, Character> tap = new HashMap<Character, Character>();
+	tap = new HashMap<>();
+	tap.put('!', 'a');tap.put('@', 'b');
+	tap.put('#', 'c');tap.put('$', 'd'); 
+	tap.put('%', 'e');tap.put('^', 'f');
+	tap.put('&', 'g');tap.put('*', 'h');
+	tap.put('(', 'i');tap.put(')', 'j');
+	tap.put('-', 'k');tap.put('_', 'l');
+	tap.put('=', 'm');tap.put('+', 'n');
+	tap.put('[', 'o');tap.put(']', 'p');
+	tap.put('{', 'q');tap.put('}', 'r');
+	tap.put('|', 's');tap.put(';', 't');
+	tap.put(':', 'u');tap.put('/', 'v');
+	tap.put('.', 'w');tap.put('ç', 'x');
+	tap.put('<', 'y');tap.put('>', 'z');
+	tap.put('?', ' ');
+	for (final char d : message.toCharArray()) {
+		//for every element in weird as an array, it does this cool stuff.
+			Character dalues = tap.get(d);
+			//exchanges the special characters with normal characters for final stuff. 
+			System.out.print(dalues);	
+			}
+}
+
 }
--- src/EnigmaTest.java
+++ src/EnigmaTest.java
@@ -1,118 +1,47 @@
 import java.util.*;
 
 public class EnigmaTest {
-	private static String message;
-
 	public static void main(String args[]) {
+		Enigma bobo = new Enigma();
+		// makes new Enigma to call methods and execute them
 		Scanner input = new Scanner(System.in);
-		System.out.println("Gimme some text to encrypt!");
-		message = input.nextLine();
-		message = "glgl " + message + " glgl";
-		// System.out.println(message);
-		String revmes = new StringBuffer(EnigmaTest.getMessage()).reverse().toString();
+		System.out.println(bobo.hello());
+		// prints hello and prompts user to input alphabets to be encrypted
+		String message = input.nextLine();
+		String glgl = bobo.glgl(message);
+		// add "glgl " to the message that confuses other people
+		String revmes = bobo.reverse(glgl);
+		// reverses message
 		revmes = revmes.toLowerCase();
-		System.out.println(revmes);
-		HashMap<Character, Character> map = new HashMap<Character, Character>();
-		map = new HashMap<>();
-		// this use hashmap method to map char a to special characters such as ! and @ and so on
-		map.put('a', '!');
-		map.put('b', '@');
-		map.put('c', '#');
-		map.put('d', '$');
-		map.put('e', '%');
-		map.put('f', '^');
-		map.put('g', '&');
-		map.put('h', '*');
-		map.put('i', '(');
-		map.put('j', ')');
-		map.put('k', '-');
-		map.put('l', '_');
-		map.put('m', '=');
-		map.put('n', '+');
-		map.put('o', '[');
-		map.put('p', ']');
-		map.put('q', '{');
-		map.put('r', '}');
-		map.put('s', '|');
-		map.put('t', ';');
-		map.put('u', ':');
-		map.put('v', '/');
-		map.put('w', '.');
-		map.put('x', 'ç');
-		map.put('y', '<');
-		map.put('z', '>');
-		map.put(' ', '?');
-		for (final char c : revmes.toCharArray()) {
-
-			char values = map.get(c);
-
-			if (values == 0) {
-				// some sorta error
-			} else {
-				// System.out.print(values + " ");
-				// prints out int values from the revmes char array which is
-				// cool
-				char bobo = values;
-				System.out.print(bobo);
-			}
-		}
-		System.out.println("\nInput the text above to decrypt!");
-		String weird = input.nextLine();
-		char[] charArray = weird.toCharArray();
-		String pototo = Arrays.toString(charArray);
-		System.out.println(pototo);
-		System.out.println("Do you want me to decrypt this gibberish?");
+		// uses .toLowerCase() method to make all the text lower case
+		bobo.encrypt(revmes);
+		// using the hashmap function to make one letter mapped to a special
+		// character
+		System.out.println("\n" + bobo.oui());
+		// asks if it wants to decrypt text
 		String potato = input.nextLine();
-		if (potato.equalsIgnoreCase("yes") || potato.equalsIgnoreCase("oui")) {
-			HashMap<Character, Character> tap = new HashMap<Character, Character>();
-			tap = new HashMap<>();
-			tap.put('!', 'a');
-			tap.put('@', 'b');
-			tap.put('#', 'c');
-			tap.put('$', 'd');
-			tap.put('%', 'e');
-			tap.put('^', 'f');
-			tap.put('&', 'g');
-			tap.put('*', 'h');
-			tap.put('(', 'i');
-			tap.put(')', 'j');
-			tap.put('-', 'k');
-			tap.put('_', 'l');
-			tap.put('=', 'm');
-			tap.put('+', 'n');
-			tap.put('[', 'o');
-			tap.put(']', 'p');
-			tap.put('{', 'q');
-			tap.put('}', 'r');
-			tap.put('|', 's');
-			tap.put(';', 't');
-			tap.put(':', 'u');
-			tap.put('/', 'v');
-			tap.put('.', 'w');
-			tap.put('ç', 'x');
-			tap.put('<', 'y');
-			tap.put('>', 'z');
-			tap.put('?', ' ');
-			for (final char d : weird.toCharArray()) {
-				//System.out.print(d);
-					Character dalues = tap.get(d);
-					//exchanges the special characters with normal characters for final stuff. 
-					System.out.print(dalues);
-				
-			}
-		//now reverse the message
-		System.out.println("\nInput the text above again pls");
-		String close = input.nextLine();
-		close = close.replace("lglg","");
-		System.out.println(close);
-		String revmes2 = new StringBuffer(close).reverse().toString();
-		System.out.println(revmes2);
+		if (potato.equalsIgnoreCase("yes") || potato.equalsIgnoreCase("oui") || potato.equalsIgnoreCase("y")) {
+			// yes or oui statement to continue with the code to decrypt
+			System.out.println("Type your encrypted text below");
+			String encrypted = input.nextLine();
+			int l = encrypted.length();
+			// finds length of the string that will be later used to remove the
+			// glgl part
+			encrypted = encrypted.substring(5, l - 5);
+			// removes glgl from the encrypted part of the code
+			bobo.decryption(encrypted);
+			// decrypts the text using hashmaps,
+			System.out.println("\nType reversed text below");
+			String reversed = input.nextLine();
+			reversed = bobo.reverse(reversed);
+			// using the same reversed method, it reverses the text.
+			System.out.println(reversed);
 		} else {
-			System.out.println("Whatcha doing with those numbers? Oh well");
+			System.out.println("Not decrypted text. Or is it?");
+			// if you say no or anything other than yes, oui, y then it is going
+			// to print this.
 		}
-	}
 
-	public static String getMessage() {
-		return EnigmaTest.message;
+		// main method ending parenthesis
 	}
 }
